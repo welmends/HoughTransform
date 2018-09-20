97f5558df9ded56413f86233ffc0d8b6e12f7a3b
well <wellmend0@gmail.com>
2018-09-19 22:17:57 -0300

    add methods to read and write PGM files

--- src/hough.cpp
+++ src/hough.cpp
@@ -16,20 +16,38 @@ typedef struct{
     unsigned char *pM;
 
 }Matrix;
-
 Matrix *image;
+
 unsigned int* accumulator;
 int accumulator_width;
 int accumulator_height;
 
 void houghTransform(unsigned char* img_data, int width, int height);
+void readImage(const char *path);
+void writeImage(const char *path);
 
 int main() {
+  image=(Matrix*)calloc(1,sizeof(Matrix));
+  readImage("images/bug.PGM");
+
+  cout<<image->rows<<endl;
+  cout<<image->cols<<endl;
+  Mat input = Mat::zeros(image->cols, image->rows, CV_8UC1);
+  for(int i=0; i<image->cols; i++){
+    for(int j=0; j<image->rows; j++){
+      input.at<uchar>(i,j) = image->pM[i*image->rows + j];
+    }
+  }
+
+  imshow("image", input);
+  waitKey();
+  writeImage("/home/well/Desktop/cu.PGM");
+  /*
   Mat input;
   Mat process;
   input = imread("images/lines.png", 0);
 	Canny(input,process,100,150,3);
-  
+
 	int width = process.cols;
 	int height = process.rows;
 
@@ -53,7 +71,7 @@ int main() {
   resize(H,H,Size(550,390));
   imshow("H", H);
   waitKey();
-
+  */
  	return 0;
 }
 
@@ -80,4 +98,41 @@ void houghTransform(unsigned char* img_data, int width, int height){
 			}
 		}
 	}
+}
+void readImage(const char *path){
+    FILE *fp;
+
+    fp = fopen(path, "r");
+    char aux[3];
+    int param1;
+    int param2;
+    int param3;
+
+    fscanf(fp,"%s",aux);
+    while(getc((fp)) == '#'){
+      while(getc((fp)) != '\n');
+    }
+    fscanf(fp, "%d %d %d",&param1,&param2,&param3);
+
+    image->rows=param1;
+    image->cols=param2;
+    image->gray=param3;
+
+    image->pM=(unsigned char*)malloc(sizeof(unsigned char)*image->rows*image->cols);
+    fread(image->pM, sizeof(unsigned char), image->rows*image->cols, fp);
+
+    fclose(fp);
+}
+void writeImage(const char *path){
+    FILE *fp;
+
+    fp = fopen(path, "w");
+
+    fprintf(fp, "P5\n");
+    fprintf(fp, "# PGM file\n");
+    fprintf(fp, "%d %d\n",image->rows, image->cols);
+    fprintf(fp, "%d\n",image->gray);
+    fwrite(image->pM, sizeof(unsigned char), image->rows*image->cols, fp);
+
+    fclose(fp);
 }
