1ff3886eed865e07aa08f4999b7c20ae55c6ea33
well <wellmend0@gmail.com>
2018-09-19 22:47:41 -0300

    refactoring code

--- src/hough.c
+++ src/hough.c
@@ -7,49 +7,49 @@ typedef struct{
     unsigned char *pM;
 }Matrix;
 
-Matrix *image;
-unsigned int* accumulator;
-int accumulator_width;
-int accumulator_height;
-
-void houghTransform(unsigned char* img_data, int width, int height);
-void readImage(const char *path);
-void writeImage(const char *path);
+void houghTransform(Matrix *image, Matrix *accumulator);
+void readImage(Matrix *image, const char *path);
+void writeImage(Matrix *image, const char *path);
 
 int main() {
-  image=(Matrix*)calloc(1,sizeof(Matrix));
-  readImage("images/line.pgm");
+  Matrix *image=(Matrix*)calloc(1,sizeof(Matrix));
+  Matrix *accumulator = (Matrix*)calloc(1,sizeof(Matrix));
 
-  houghTransform(image->pM, image->cols, image->rows);
+  readImage(image, "images/line.pgm");
+
+  houghTransform(image, accumulator);
+
+  writeImage(image, "accu.pgm");
 
  	return 0;
 }
 
-void houghTransform(unsigned char* img_data, int width, int height){
+void houghTransform(Matrix *image, Matrix *accumulator){
 	//accu_height = 2*D and D = sqrt(height^2 + width^2)
-	accumulator_height = ceil(2*(sqrt(height*height + width*width)));
-	accumulator_width  = 180;
-	accumulator        = (unsigned int*)calloc(accumulator_height*accumulator_width, sizeof(unsigned int));
+	accumulator->rows  = ceil(2*(sqrt(image->rows*image->rows + image->cols*image->cols)));
+	accumulator->cols  = 180;
+  accumulator->gray  = 255;
+	accumulator->pM    = (unsigned char*)calloc(accumulator->rows*accumulator->cols, sizeof(unsigned char));
 
 	double rho;
 	int theta,i,j;
-	for(j=0; j<height; j++){
-		for(i=0; i<width; i++){
-			if(img_data[ (j*width) + i] == 255){
+	for(j=0; j<image->rows; j++){
+		for(i=0; i<image->cols; i++){
+			if(image->pM[ (j*image->cols) + i] == 255){
 				for(theta=0; theta<180; theta++){
 					// rho = xcos(theta) + ysin(theta)
 					rho = ( (i)*cos(theta*M_PI/180)  ) + ( (j)*sin(theta*M_PI/180) );
 					// accumulator(theta,rho+D)++
-          if((int)((ceil(rho + accumulator_height/2) * 180.0)) + theta>0 &&
-             (int)((ceil(rho + accumulator_height/2) * 180.0)) + theta<accumulator_height*accumulator_width){
-               	accumulator[ (int)((ceil(rho + accumulator_height/2) * 180.0)) + theta]++;
+          if((int)((ceil(rho + accumulator->rows/2) * 180.0)) + theta>0 &&
+             (int)((ceil(rho + accumulator->rows/2) * 180.0)) + theta<accumulator->rows*accumulator->cols){
+               	accumulator->pM[ (int)((ceil(rho + accumulator->rows/2) * 180.0)) + theta]++;
           }
 				}
 			}
 		}
 	}
 }
-void readImage(const char *path){
+void readImage(Matrix *image, const char *path){
     FILE *fp;
 
     fp = fopen(path, "r");
@@ -73,7 +73,7 @@ void readImage(const char *path){
 
     fclose(fp);
 }
-void writeImage(const char *path){
+void writeImage(Matrix *image, const char *path){
     FILE *fp;
 
     fp = fopen(path, "w");
