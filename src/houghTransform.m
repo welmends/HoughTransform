930e5fc929bcad533c006673b873e5fa9e73371b
well <wellmend0@gmail.com>
2018-09-20 23:42:19 -0300

    add some images of matlab validation

--- src/houghTransform.m
+++ src/houghTransform.m
@@ -2,16 +2,16 @@ clear all
 close all
 clc
 
-I = imread('../images/png/building.png');
-I  = rgb2gray(I);
+I = imread('../images/png/line.png');
+%I  = rgb2gray(I);
 
 BW = edge(I,'canny');
 [H,Theta,Rho] = hough(BW);
 
  figure
  %imshow(rescale(H),'XData',Theta,'YData',Rho,'InitialMagnification','fit');
- imshow(rescale(H));
- %imshow(H);
+ %imshow(rescale(H));
+ imshow(H);
  title('Hough Transform Matrix');
  xlabel('\theta')
  ylabel('\rho');
