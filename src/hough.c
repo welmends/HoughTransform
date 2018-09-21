ad16d863675c22afa3454f22b4f0f0f43f64bf4a
well <wellmend0@gmail.com>
2018-09-21 00:25:11 -0300

    add documentation to all methods

--- src/hough.c
+++ src/hough.c
@@ -1,9 +1,20 @@
+/**
+ * @file hough.c
+ * @brief Hough Transform Implemantation
+          Input: Path to an image, Type of the image (Canny or Original) and Output type (Binary or 8 Bits)
+          Output: Hough Transform Matrix (Accumulator)
+ * @author $Author:$ de Souza, Joao Wellington Mendes; Brito, Messyo Sousa
+ * @version $Revision:$
+ * @date $Date:$ 21/09/2018
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
-#define IMAGE_PATH_SIZE 100
+#define IMAGE_PATH_SIZE (100)
+#define THRESH_VALUE    (200)
 
 typedef struct{
     int rows,cols,gray;
@@ -16,6 +27,14 @@ void readImage(Matrix *image, const char *path);
 void writeImage(Matrix *image, const char *path);
 void houghTransform(Matrix *image, Matrix *accumulator, char out_type);
 
+/**
+ *  Function: main
+ *  Input: int argc, char **argv
+ *  Output: int
+ *  Purpose: The main method will call all other methods
+ *  aiming to perform all operations prescribed. Also,
+ *  the main method receives as input the arguments.
+ */
 int main(int argc, char **argv) {
   if(argc!=4){
     instructions();
@@ -33,7 +52,14 @@ int main(int argc, char **argv) {
   }
 }
 
-void instructions(){
+/**
+ *  Function: instructions
+ *  Input: ---
+ *  Output: void
+ *  Purpose: Print on screen the instructions to use this
+ *  program.
+ */
+void instructions(void){
   printf("\n");
   printf(" **************************************************** \n");
   printf("|                   [Instructions]                   |\n");
@@ -49,6 +75,14 @@ void instructions(){
   printf(" **************************************************** \n");
   printf("\n");
 }
+
+/**
+ *  Function: getImagePath
+ *  Input: char **argv, char *out_type
+ *  Output: char*
+ *  Purpose: Verifies the type arguments and returns a path to
+ *  read a specifc image.
+ */
 char* getImagePath(char **argv, char *out_type){
   if(strcmp(argv[1],"canny")!=0 && strcmp(argv[1],"original")!=0){
     printf("Error: <type> must be canny or original\n");
@@ -73,14 +107,21 @@ char* getImagePath(char **argv, char *out_type){
   strcat(imagePath, ".pgm");
   return imagePath;
 }
+
+/**
+ *  Function: readImage
+ *  Input: Matrix *image, const char *path
+ *  Output: void
+ *  Purpose: Since the path to a specific image was passed to
+ *  this method, this function reads the header and the PGM
+ *  image and stores this at the Matrix Structure passed as
+ *  input to this method.
+ */
 void readImage(Matrix *image, const char *path){
     FILE *fp;
-
     fp = fopen(path, "r");
     char aux[3];
-    int param1;
-    int param2;
-    int param3;
+    int param1,param2,param3;
 
     fscanf(fp,"%s",aux);
     while(getc((fp)) == '#'){
@@ -96,9 +137,17 @@ void readImage(Matrix *image, const char *path){
 
     fclose(fp);
 }
+
+/**
+ *  Function: writeImage
+ *  Input: Matrix *image, const char *path
+ *  Output: void
+ *  Purpose: Writes the Matrix Structure passed as input to
+ *  this method in a specific path that was also passed as
+ *  input to this method.
+ */
 void writeImage(Matrix *image, const char *path){
     FILE *fp;
-
     fp = fopen(path, "w");
 
     fprintf(fp, "P5\n");
@@ -109,6 +158,19 @@ void writeImage(Matrix *image, const char *path){
 
     fclose(fp);
 }
+
+/**
+ *  Function: houghTransform
+ *  Input: Matrix *image, Matrix *accumulator, char out_type
+ *  Output: void
+ *  Purpose: This function performs all the calculus related
+ *  to the Hough Transform. This occurs from the image scanning
+ *  and calculation of the rho = xcos (theta) + ysin (theta)
+ *  [0 < theta <180] for pixels that display the high level.
+ *  It is worth mentioning that if the image passed as an
+ *  input is not binarized, a threshold will be applied to
+ *  it with a default threshold value set in THRESH_VALUE.
+ */
 void houghTransform(Matrix *image, Matrix *accumulator, char out_type){
 	//accu_height = 2*D and D = sqrt(height^2 + width^2)
 	accumulator->cols  = ceil(2*(sqrt(image->rows*image->rows + image->cols*image->cols))) - 1;
@@ -120,7 +182,7 @@ void houghTransform(Matrix *image, Matrix *accumulator, char out_type){
 	int theta,i,j;
 	for(j=0; j<image->cols; j++){
 		for(i=0; i<image->rows; i++){
-			if(image->pM[ (j*image->rows) + i] == 255){
+			if(image->pM[ (j*image->rows) + i] > THRESH_VALUE){
 				for(theta=0; theta<180; theta++){
 					// rho = xcos(theta) + ysin(theta)
 					rho = ( (j)*cos(theta*M_PI/180)  ) + ( (i)*sin(theta*M_PI/180) );
